a40ae20f2e5535843095a824b4e90c269942ac50
Unknown <fgroch@gmail.com>
2017-01-05 16:53:29 +0100

    Controller methods implementation

--- grails-app/controllers/tools/blocks/flowable/FlowableRepositoryController.groovy
+++ grails-app/controllers/tools/blocks/flowable/FlowableRepositoryController.groovy
@@ -1,6 +1,39 @@
 package tools.blocks.flowable
 
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest
+
+import static org.springframework.http.HttpStatus.NO_CONTENT
+
 class FlowableRepositoryController {
 
-    def index() { }
+    def flowableRepositoryService
+
+    def deploymentsCount() {
+        flowableRepositoryService.deploymentsCount()
+    }
+
+    def getDeployments() {
+        params.offset = params.offset ?: 0
+        params.max = Math.min(max ?: 10, 100)
+        flowableRepositoryService.getDeployments(params.offset, params.max)
+    }
+
+    def deploy() {
+        if (!params.file || !(params.file instanceof StandardMultipartHttpServletRequest.StandardMultipartFile)) {
+            notFound()
+            return
+        }
+        def deployment = flowableRepositoryService.deploy(params.file, params.name ?: '')
+        respond(deployment: deployment)
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'flowable.empty.upload.deployment', default: 'No file attached as deployment')
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NO_CONTENT, text: message(code: 'flowable.empty.upload.deployment', default: 'No file attached as deployment') }
+        }
+    }
 }

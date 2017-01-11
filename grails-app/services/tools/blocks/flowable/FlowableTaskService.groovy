6915cb91d788fc171983150b0e7ef0be1c1b3996
Unknown <fgroch@gmail.com>
2017-01-11 18:17:28 +0100

    Claim, complete, resolve,  completeWithForm methods implementation

--- grails-app/services/tools/blocks/flowable/FlowableTaskService.groovy
+++ grails-app/services/tools/blocks/flowable/FlowableTaskService.groovy
@@ -1,6 +1,7 @@
 package tools.blocks.flowable
 
 import grails.transaction.Transactional
+import org.flowable.engine.FlowableTaskAlreadyClaimedException
 import org.flowable.engine.TaskService
 import org.flowable.engine.common.api.FlowableException
 import org.flowable.engine.common.api.FlowableObjectNotFoundException
@@ -75,5 +76,104 @@ class FlowableTaskService {
             return false
         }
         return true
+    }
+
+    boolean claim(String taskId, String userId) {
+        try {
+            taskService.claim(taskId, userId)
+        } catch (FlowableException | FlowableTaskAlreadyClaimedException e) {
+            return false
+        }
+        return true
+    }
+
+    boolean claim(String taskId) {
+        try {
+            taskService.unclaim(taskId)
+        } catch (FlowableObjectNotFoundException e) {
+            return false
+        }
+        return true
+    }
+
+    boolean complete(String taskId) {
+        complete(taskId, null)
+    }
+
+    boolean delegateTask(String taskId, String userId) {
+        try {
+            taskService.delegateTask(taskId, userId)
+        } catch (FlowableObjectNotFoundException e) {
+            return false
+        }
+        return true
+    }
+
+    boolean resolveTask(String taskId) {
+        try {
+            taskService.resolveTask(taskId)
+        } catch (FlowableObjectNotFoundException e) {
+            return false
+        }
+        return true
+    }
+
+    boolean resolveTask(String taskId, Map<String, Object> variables) {
+        try {
+            taskService.resolveTask(taskId, variables)
+        } catch (FlowableObjectNotFoundException e) {
+            return false
+        }
+        return true
+    }
+
+    boolean resolveTask(String taskId, Map<String, Object> variables, Map<String, Object> transientVariables) {
+        try {
+            taskService.resolveTask(taskId, variables, transientVariables)
+        } catch (FlowableObjectNotFoundException e) {
+            return false
+        }
+        return true
+    }
+
+    boolean complete(String taskId, Map<String, Object> variables) {
+        try {
+            taskService.complete(taskId, variables)
+        } catch (FlowableObjectNotFoundException | FlowableObjectNotFoundException e) {
+            return false
+        }
+        return true
+    }
+
+    boolean complete(String taskId, Map<String, Object> variables, Map<String, Object> transientVariables) {
+        try {
+            taskService.complete(taskId, variables, transientVariables)
+        } catch (FlowableObjectNotFoundException e) {
+            return false
+        }
+        return true
+    }
+
+    boolean complete(String taskId, Map<String, Object> variables, boolean localScope) {
+        try {
+            taskService.complete(taskId, variables, localScope)
+        } catch (FlowableObjectNotFoundException e) {
+            return false
+        }
+        return true
+    }
+
+    def completeTaskWithForm(String taskId, String formDefinitionId, String outcome, Map<String, Object> variables) {
+        taskService.completeTaskWithForm(taskId, formDefinitionId, outcome, variables)
+    }
+
+    def completeTaskWithForm(String taskId, String formDefinitionId, String outcome,
+                             Map<String, Object> variables, Map<String, Object> transientVariables) {
+        taskService.completeTaskWithForm(taskId, formDefinitionId, outcome, variables, transientVariables)
+    }
+
+    def completeTaskWithForm(String taskId, String formDefinitionId, String outcome,
+                             Map<String, Object> variables, boolean localScope) {
+        taskService.completeTaskWithForm(taskId, formDefinitionId, outcome, variables, localScope)
     }
 }

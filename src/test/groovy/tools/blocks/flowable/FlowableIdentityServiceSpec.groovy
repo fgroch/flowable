ce87556bcc6ed75a9bd7b1b321cec836baf27933
Unknown <fgroch@gmail.com>
2017-01-13 02:04:16 +0100

    Some tests added

--- src/test/groovy/tools/blocks/flowable/FlowableIdentityServiceSpec.groovy
+++ src/test/groovy/tools/blocks/flowable/FlowableIdentityServiceSpec.groovy
@@ -1,6 +1,8 @@
 package tools.blocks.flowable
 
 import grails.test.mixin.TestFor
+import org.flowable.engine.IdentityService
+import org.flowable.idm.api.User
 import spock.lang.Specification
 
 /**
@@ -9,14 +11,27 @@ import spock.lang.Specification
 @TestFor(FlowableIdentityService)
 class FlowableIdentityServiceSpec extends Specification {
 
+    IdentityService identityService
+
     def setup() {
+        identityService = Mock()
+        service.identityService = identityService
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    def "when service is created identity service is created"() {
+        expect:
+            service.identityService != null
+    }
+
+    def "when creating user a new user object is created"() {
+        when:
+            User user = service.newUser("test")
+        then:
+            service.identityService != null
+            user != null
+            user.id == "test"
     }
 }

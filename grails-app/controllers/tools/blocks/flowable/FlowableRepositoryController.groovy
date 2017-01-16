89fdfaa40de49f4156fc6b62626cd6a994120321
Unknown <fgroch@gmail.com>
2017-01-17 00:25:06 +0100

    Small bugfix in names

--- grails-app/controllers/tools/blocks/flowable/FlowableRepositoryController.groovy
+++ grails-app/controllers/tools/blocks/flowable/FlowableRepositoryController.groovy
@@ -24,14 +24,14 @@ class FlowableRepositoryController {
 
     @Transactional
     def deploy() {
-        if (!params.deplymentFile || !(params.deplymentFile instanceof StandardMultipartHttpServletRequest.StandardMultipartFile)) {
+        if (!params.deploymentFile || !(params.deploymentFile instanceof StandardMultipartHttpServletRequest.StandardMultipartFile)) {
             emptyDeployment()
             return
         }
         def key = params.key ?: null
         def category = params.category ?: null
         String name = params.name ?: ''
-        def deployment = flowableRepositoryService.deploy(params.deplymentFile, name, category, key)
+        def deployment = flowableRepositoryService.deploy(params.deploymentFile, name, category, key)
         respond(deployment: deployment)
     }
 

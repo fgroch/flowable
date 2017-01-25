729f522b0f7c93fa1646faa6fd9e443db27cef76
Unknown <fgroch@gmail.com>
2017-01-25 23:57:29 +0100

    Two more test

--- src/test/groovy/tools/blocks/flowable/FlowableTagLibSpec.groovy
+++ src/test/groovy/tools/blocks/flowable/FlowableTagLibSpec.groovy
@@ -28,4 +28,18 @@ class FlowableTagLibSpec extends Specification {
         expect:
         tagLib.claimTaskButton(attr, null).toString() == "<a class='btn btn-default btn-claim ' href='/flowableTask/claimTask?taskId=1&userId=test'> <span class='fa fa-test'></span></a>"
     }
+
+    void "unclaimTaskButton as method"() {
+        given:
+        def attr = [taskId:1, iconClass:"fa fa-test", userId:"test"]
+        expect:
+        tagLib.unclaimTaskButton(attr, null).toString() == "<a class='btn btn-default btn-unclaim ' href='/flowableTask/unclaimTask?taskId=1'> <span class='fa fa-test'></span></a>"
+    }
+
+    void "completeTask as method"() {
+        given:
+        def attr = [taskId:1, iconClass:"fa fa-test"]
+        expect:
+        tagLib.completeTaskButton(attr, null).toString() == "<a class='btn btn-default btn-complete ' href='/flowableTask/completeTask?taskId=1'>  <span class='fa fa-test'></span></a>"
+    }
 }

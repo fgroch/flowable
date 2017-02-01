e83047ead0ee4a0e96c5c317e6719b1aa911f06c
Unknown <fgroch@gmail.com>
2017-02-02 00:00:29 +0100

    Runtime service integration tests initial commit

--- src/integration-test/groovy/tools/blocks/FlowableRuntimeServiceIntegrationSpec.groovy
+++ src/integration-test/groovy/tools/blocks/FlowableRuntimeServiceIntegrationSpec.groovy
@@ -4,21 +4,20 @@ import grails.config.Config
 import grails.test.mixin.integration.Integration
 import grails.transaction.Rollback
 import org.flowable.engine.repository.Deployment
-import org.flowable.engine.repository.DiagramLayout
 import org.flowable.engine.repository.ProcessDefinition
 import org.grails.datastore.gorm.jdbc.DataSourceBuilder
 import org.grails.io.support.ClassPathResource
 import org.grails.io.support.Resource
 import spock.lang.Specification
 import tools.blocks.flowable.FlowableRepositoryService
+import tools.blocks.flowable.FlowableRuntimeService
 
 /**
- * Created by fgroch on 29.01.17.
+ * Created by fgroch on 01.02.17.
  */
 @Integration
 @Rollback
-class FlowableRepositoryServiceIntegrationSpec extends Specification {
-
+class FlowableRuntimeServiceIntegrationSpec extends Specification {
     static loadExternalBeans = true
 
     def doWithSpring = {
@@ -48,10 +47,12 @@ class FlowableRepositoryServiceIntegrationSpec extends Specification {
             processEngineConfiguration = processEngineConfiguration
         }
         repositoryService(processEngine: "getRepositoryService")
+        runtimeService(processEngine: "getRuntimeService")
 
     }
 
     FlowableRepositoryService flowableRepositoryService
+    FlowableRuntimeService flowableRuntimeService
     Resource r
     Deployment deployment
     ProcessDefinition processDefinition
@@ -67,81 +68,14 @@ class FlowableRepositoryServiceIntegrationSpec extends Specification {
         deploymentKey = deployment.key
         deploymentId = deployment.id
         processDefinition = flowableRepositoryService.createProcessDefinitionQuery().processDefinitionKey("testX").singleResult()
-        //System.println("--deploymentKey:" + deploymentKey + "--")
     }
 
     def cleanup() {
 
     }
 
-    def "when service is created repository service is created"() {
+    def "when service is created runtime service is created"() {
         expect:
-        flowableRepositoryService.repositoryService != null
+            flowableRuntimeService.runtimeService != null
     }
-
-    def "when process definition is deployed deployment id won't be null"() {
-        when:
-            deployProcess()
-            deploymentId = deployment.id
-            System.println(deploymentId)
-        then:
-            deploymentId != null
-    }
-
-    def "when process definition is deployed, deployments count should be greater than before deploy"() {
-        when:
-            def cntBefore = flowableRepositoryService.deploymentsCount()
-            System.println(cntBefore)
-            deployProcess()
-            def cntAfter = flowableRepositoryService.deploymentsCount()
-            System.println(cntAfter)
-        then:
-            cntAfter > cntBefore
-    }
-
-    def "when process definition is suspended then isProcessDefinitionSuspended service method should return true"() {
-        when:
-            deployProcess()
-            flowableRepositoryService.suspendProcessDefinitionById(processDefinition.id)
-            boolean isSuspended = flowableRepositoryService.isProcessDefinitionSuspended(processDefinition.id)
-        then:
-            isSuspended
-    }
-
-    def "when process definition is suspended then after activation isProcessDefinitionSuspended service method should return false"() {
-        when:
-            deployProcess()
-            flowableRepositoryService.suspendProcessDefinitionById(processDefinition.id)
-            boolean isSuspendedBefore = flowableRepositoryService.isProcessDefinitionSuspended(processDefinition.id)
-            flowableRepositoryService.activateProcessDefinitionById(processDefinition.id)
-            boolean isSuspendedAfter = flowableRepositoryService.isProcessDefinitionSuspended(processDefinition.id)
-        then:
-            isSuspendedBefore && !isSuspendedAfter
-    }
-
-    def "when process definition is deployed then process model should not be null"() {
-        when:
-            deployProcess()
-            InputStream is = flowableRepositoryService.getProcessModel(processDefinition.id)
-        then:
-            is != null && is.available() > 0
-    }
-
-    def "when process definition is deployed then process diagram should not be null"() {
-        when:
-            deployProcess()
-            InputStream is = flowableRepositoryService.getProcessDiagramResource(processDefinition.key)
-        then:
-            is != null && is.available() > 0
-    }
-
-    def "when process definition is deployed then process diagram layout should not be null"() {
-        when:
-            deployProcess()
-            DiagramLayout diagramLayout = flowableRepositoryService.getProcessDiagramLayout(processDefinition.id)
-        then:
-            diagramLayout != null
-    }
-
-    //getProcessDiagramLayout
 }
